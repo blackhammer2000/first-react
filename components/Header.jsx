be7604d01fc95840d2d231b349abe25ba01c932b
wawerusamuel <waweruzamuel@gmail.com>
2022-08-22 23:09:39 +0300

    first react app

---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./css/header.css";
+import "./css/bootstrap.min.css";
+
+const Header = () => {
+  return (
+    <div className="container text-center">
+      <h3>Header</h3>
+    </div>
+  );
+};
+
+export default Header;
